a4c4aca2ae0fcbaf52e5f980e2467f821e546354
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-27 15:06:04 +0000

    What can be used front of variable name?

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Im Working. Im js. Im worth it');
+// What can be used front of variable name?
+// 1. let
+// When we want to change 33 to something, we use let.
+let a = 33;
+
+// 2. const
+// When we want to do not change anymore variable, we use const.
+const pi = 3.141592;
+// pi = 3;    ->  It will be error.
+
+// 3. var = variable
+// It doesn't matter now, but sometimes it will be matter.
+var b = 22;
