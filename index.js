da592a7e5a3dadfa498657f69920de3dde0487e2
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-27 15:13:55 +0000

    Let's learn about variable data type!

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ const pi = 3.141592;
 // 3. var = variable
 // It doesn't matter now, but sometimes it will be matter.
 var b = 22;
+
+// Use const default
+// Use let only you need it  
+
+// Let's learn about variable data type
+// 1. String
+const what = 'juli';
+console.log(what);
+
+// 2. Boolean
+// You can save just true or false
+const bool = true;
+
+// 3. Number
+const num = 12345;
+
+// 4. Float
+const fl = 33.1;
