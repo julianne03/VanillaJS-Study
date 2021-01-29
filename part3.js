947e77100af65017a1121a8c7d96012732e9cb75
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-29 12:48:56 +0000

    #1. JS DOM function!

---
+++ part3.js
@@ -0,0 +1,8 @@
+// How can we get id from html file?
+// HTML document = javascript
+const title = document.getElementById('title');
+// DOM : Document Object Model
+// javascript -> html change to object
+
+// It will be change to h1 context
+title.innerHTML = "Hi! From JS";
