fb66459957a67b5f1bddfd66dfc3fd94104c8fea
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-29 08:23:10 +0000

    #1. My first JS Function

---
+++ part2.js
@@ -0,0 +1,13 @@
+// function
+// You can put peice of codes in here as many times as you want
+// argument = variable
+//          = we sent something to function 
+function sayHello(potato, age) {  // parameter - 매개변수
+  console.log('Hello!', potato, "you have", age, 'years of age.');
+}
+
+// call function
+sayHello("julianne", 15);   // argument - 인자
+console.log("Hi!")
+// built in function (내장함수)
+// ex. alert, console
