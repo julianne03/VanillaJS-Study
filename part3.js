3ae608d71c842a0530b05f9784e230b58a611662
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-29 12:59:42 +0000

    #1. Modifying the DOM with JS!

--- part3.js
+++ part3.js
@@ -1,8 +1,14 @@
 // How can we get id from html file?
 // HTML document = javascript
-const title = document.getElementById('title');
+// If you want to get id -> getElementbyId
+// querySelector - class use dot(.), id use # (like css)
+const title = document.querySelector('#title');
 // DOM : Document Object Model
 // javascript -> html change to object
 
 // It will be change to h1 context
 title.innerHTML = "Hi! From JS";
+
+// Modifying the DOM with JS
+title.style.color = 'red';
+document.title = "I Own you now.";
