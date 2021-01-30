0497f423f75d884eb107a5222fa8598379854028
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-30 08:00:34 +0000

    DOM - Function practice with if-else! (part 1)

--- part3.js
+++ part3.js
@@ -6,20 +6,40 @@ const title = document.querySelector('#title');
 // DOM : Document Object Model
 // javascript -> html change to object
 
+const BASE_COLOR = "white";
+const OTHER_COLOR = "black";
 // It will be change to h1 context
 title.innerHTML = "Hi! From JS";
 
 // Modifying the DOM with JS
-title.style.color = 'red';
 document.title = "I Own you now.";
 
 // events and event handler
 // 1. make a function
 function handleClick() {
-  title.style.color = "blue";
+  const  currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
+
 // 2. make a eventhandler
 // first argument : type of event
 // second argument : function name - without () !!!!
 // If you with (), it means you call function now!
-title.addEventListener("click", handleClick);
+// You can know more about dom at MDN site
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+init();
+function handleOffLine() {
+  console.log('Bye Bye!')
+}
+function handleOnLine() {
+  console.log('Welcome back!')
+}
+window.addEventListener("offline", handleOffLine);
+window.addEventListener("online", handleOnLine);
