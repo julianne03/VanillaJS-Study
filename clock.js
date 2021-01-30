e0df1858ee4be0a7f2d4b426d5d1c1dc7269684c
JULIANNESEUNGMIN YI <s2019w33@e-mirim.hs.kr>
2021-01-30 12:40:05 +0000

    #2. Making real time clock!

--- clock.js
+++ clock.js
@@ -2,13 +2,21 @@ const clockContainer = document.querySelector('.js-clock'),
       clockTitle = document.querySelector("h1");
 
 function getTime() {
-  const date = new Date();
+  const date = new Date();   // Object
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+  clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes
+    }:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
+
 function init() {
+  // first, get time from now
   getTime();
+  // second, use function setInterval
+  // => we can get time for every second(1000)
+  // call getTime for every second
+  setInterval(getTime, 1000);
 }
 init();
