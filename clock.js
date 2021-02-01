b6b1e91cce370df67ac464899395d0eb69fcc88f
s2019w33@e-mirim.hs.kr <s2019w33@e-mirim.hs.kr>
2021-02-01 14:11:09 +0900

    #3. delete seconds from clock!

--- clock.js
+++ clock.js
@@ -1,14 +1,13 @@
 const clockContainer = document.querySelector('.js-clock'),
-      clockTitle = document.querySelector("h1");
+      clockTitle = document.querySelector(".js-clock-title");
 
 function getTime() {
   const date = new Date();   // Object
   const minutes = date.getMinutes();
   const hours = date.getHours();
-  const seconds = date.getSeconds();
   clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }`;
 }
 
 function init() {
